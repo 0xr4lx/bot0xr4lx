a5710f2d1bdd42fce564dd6c927781ca215ec535
0xr4lx <renxv86@gmail.com>
2025-04-22 22:49:26 -0700

    Update brat.py

--- PyroUbot/modules/brat.py
+++ PyroUbot/modules/brat.py
@@ -10,17 +10,13 @@ __HELP__ =  """
 ᚗ <code>{0}brat [text]</code>
 ⊷ Untuk Membuat Gambar Text Seperti Tren Tiktok</b></blockquote>
 
-<blockquote><b>⎆ ᴘᴇʀɪɴᴛᴀʜ:
-ᚗ <code>{0}bratvideo [text]</code>
-⊷ Untuk Membuat Gambar Text video Seperti Tren Tiktok</b></blockquote>
-
 """
 
 def get_brat_image(text):
     url = "https://api.botcahx.eu.org/api/maker/brat"
     params = {
         "text": text,
-        "apikey": "moire"
+        "apikey": "0xr4lx"
     }
     try:
         response = requests.get(url, params=params)
@@ -48,45 +44,6 @@ async def _(client, message):
         temp_file = "img.jpg"
         with open(temp_file, "wb") as f:
             f.write(image_content)
-
-        await message.reply_photo(photo=temp_file)
-        
-        os.remove(temp_file)
-    else:
-        await message.reply_text("apikey sedang bermasalah....")
-
-def get_brat_video(text):
-    url = "https://api.botcahx.eu.org/api/maker/brat-video"
-    params = {
-        "text": text,
-        "apikey": "VENOZY"
-    }
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        
-        if response.headers.get("Content-Type", "").startswith("image/"):
-            return response.content
-        else:
-            return None
-    except requests.exceptions.RequestException:
-        return None
-        
-@PY.UBOT("bratvideox")
-async def _(client, message):
-    args = message.text.split(" ", 1)
-    if len(args) < 2:
-        await message.reply_text("gunakan perintah .bratvideo <teks> untuk membuat gambar.")
-        return
-
-    request_text = args[1]
-    await message.reply_text("sedang memproses, mohon tunggu...")
-
-    mp4_content = get_brat_video(request_text)
-    if mp4_content:
-        temp_file = "img.mp4"
-        with open(temp_file, "wb") as f:
-            f.write(mp4_content)
 
         await message.reply_photo(photo=temp_file)
         
