0eca1c9414114bb72d05ac9d96518eb8ae2c7075
0xr4lx <renxv86@gmail.com>
2025-04-25 19:14:28 -0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -7,11 +7,11 @@ MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
 DEVS = list(map(int, os.getenv("DEVS", "6818931531").split()))
 
-API_ID = int(os.getenv("API_ID", "27163201"))
+API_ID = int(os.getenv("API_ID", "your ip id"))
 
-API_HASH = os.getenv("API_HASH", "4465a935288c78e764d9f6a8f060ca79")
+API_HASH = os.getenv("API_HASH", "your api hash")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7652775778:AAFoon5IDfQfmcWcS1XWEZJ5quSRrsXDIi0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "Your bot token")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "6818931531"))
 
@@ -21,4 +21,4 @@ RMBG_API = os.getenv("RMBG_API", "f3CYy31QTGdSyYABY8D7FTqQ")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Linxxxx:Linxx2005@cluster0.ea9bybg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002663436301"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-your logs "))
