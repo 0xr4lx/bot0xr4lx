ae2a1b4454a71406a4b32a743a378efb047186f2
0xr4lx <renxv86@gmail.com>
2025-04-27 00:44:18 -0700

    Update broadcastbot.py

--- PyroUbot/modules/broadcastbot.py
+++ PyroUbot/modules/broadcastbot.py
@@ -3,8 +3,25 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pymongo import MongoClient
+from PyroUbot.config import MONGO_URL
 from PyroUbot.core.helpers.msg_type import ReplyCheck
 from PyroUbot import *
+
+client = MongoClient(MONGO_URL)
+db = client["PyroUbot"]
+users_col = db["users"]  
+
+def add_user(user_id: int):
+    users_col.update_one(
+        {"_id": user_id},
+        {"$set": {"_id": user_id}},
+        upsert=True,
+    )
+
+def get_all_users():
+    users = users_col.find().to_list(length=None)
+    return [user["_id"] for user in users]
 
 @ubot.on_message(filters.command("unprem") & filters.me)
 async def jwbsalamlngkp(client: Client, message: Message):
@@ -32,11 +49,14 @@ async def broadcast_bot(client, message):
     done = 0
     if not message.reply_to_message:
         return await msg.edit("<b>⌭ ᴍᴏʜᴏɴ ʙᴀʟᴀs ᴘᴇsᴀɴ</b>")
-    for x in ubot._ubot:
+    
+    users = get_all_users()
+    for user_id in users:
         try:
-            await x.unblock_user(bot.me.username)
-            await message.reply_to_message.forward(x.me.id)
+            await message.reply_to_message.copy(user_id)
             done += 1
+            await asyncio.sleep(0.05)
         except Exception:
             pass
     return await msg.edit(f"⌭ ʙᴇʀʜᴀsɪʟ ᴍᴇɴɢɪʀɪᴍ ᴘᴇsᴀɴ ᴋᴇ {done} ᴜʙᴏᴛ")
+
